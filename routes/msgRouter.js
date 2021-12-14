52dd89806ece040a543b61e2683468dea3edf990
강덕현 <54894853+dukhyeonkang@users.noreply.github.com>
2021-12-14 10:10:53 +0900

    ssss
    ssss

--- routes/msgRouter.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/msg', function(req, res, next) {
-  res.send("This is[ /msg ]directroy");
-});
-
-module.exports = router;
