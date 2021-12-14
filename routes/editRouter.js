b4fc213fe8eefa14eb0dbb41028354ebfd9ec9cf
강덕현 <54894853+dukhyeonkang@users.noreply.github.com>
2021-12-14 20:33:27 +0900

    delete
    delete

--- routes/editRouter.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/edit', function(req, res, next) {
-  res.send("This is[ /edit ]directroy");
-});
-
-module.exports = router;
